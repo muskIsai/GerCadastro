43a83682a50ca549a7b1ee24c8391f50fb8e3fc6
Isaías A. Carlos <43414375+muskIsai@users.noreply.github.com>
2019-01-22 06:06:14 +0300

    ModelClient

---
+++ GerApp/GerApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GerApp.MvcApplication" Language="C#" %>
---
+++ GerApp/GerApp/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GerApp.Models
+{
+    public class Client
+    {
+        public int ClientID { get; set; }
+        public string Nome { get; set; }
+        public int Passaport { get; set; }
+    }
+}
