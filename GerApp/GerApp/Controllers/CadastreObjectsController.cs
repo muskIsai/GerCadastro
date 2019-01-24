fe0ee05eb3534c429aec6f3b8c8d111cf3461e36
Isaías A. Carlos <43414375+muskIsai@users.noreply.github.com>
2019-01-24 23:57:49 +0300

    Changes ControlCadastre

--- GerApp/GerApp/Controllers/CadastreObjectsController.cs
+++ GerApp/GerApp/Controllers/CadastreObjectsController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using GerApp.Models;
@@ -9,13 +12,117 @@ namespace GerApp.Controllers
 {
     public class CadastreObjectsController : Controller
     {
-        private EFContext conte = new EFContext();
+        private EFContext db = new EFContext();
 
         // GET: CadastreObjects
         public ActionResult Index()
         {
-            //Vai ordenar(mostrar) a lista em funcao do tipo de cadastro
-            return View(conte.CadastreObjects.OrderBy(x => x.TypeCadastre)); 
+            //return View(db.CadastreObjects.ToList());
+            return View(db.CadastreObjects.OrderBy(x => x.TypeCadastre));
+        }
+
+        // GET: CadastreObjects/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CadastreObjects cadastreObjects = db.CadastreObjects.Find(id);
+            if (cadastreObjects == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cadastreObjects);
+        }
+
+        // GET: CadastreObjects/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CadastreObjects/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CadastreObjectsID,Price,TypeCadastre,RegistrationDate,Comment")] CadastreObjects cadastreObjects)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CadastreObjects.Add(cadastreObjects);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cadastreObjects);
+        }
+
+        // GET: CadastreObjects/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CadastreObjects cadastreObjects = db.CadastreObjects.Find(id);
+            if (cadastreObjects == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cadastreObjects);
+        }
+
+        // POST: CadastreObjects/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CadastreObjectsID,Price,TypeCadastre,RegistrationDate,Comment")] CadastreObjects cadastreObjects)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cadastreObjects).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cadastreObjects);
+        }
+
+        // GET: CadastreObjects/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CadastreObjects cadastreObjects = db.CadastreObjects.Find(id);
+            if (cadastreObjects == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cadastreObjects);
+        }
+
+        // POST: CadastreObjects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CadastreObjects cadastreObjects = db.CadastreObjects.Find(id);
+            db.CadastreObjects.Remove(cadastreObjects);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
--- GerApp/GerApp/Models/CadastreObjects.cs
+++ GerApp/GerApp/Models/CadastreObjects.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,15 @@ namespace GerApp.Models
 {
     public partial class CadastreObjects
     {
+        [Display(Name = "ID")]
         public int CadastreObjectsID { get; set; }
+        [Display(Name = "Cтоимость")]
         public decimal Price { get; set; }
+        [Display(Name = "Тип кадастра")]
         public string TypeCadastre { get; set; }
+        [Display(Name = "Дата доставка на учет")]
         public DateTime RegistrationDate { get; set; }
+        [Display(Name = "Коментарый")]
         public string Comment { get; set; }
     }
 }
